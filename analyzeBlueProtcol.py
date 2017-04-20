b264f3189e5c2a2913a7ce8e9b551c96dc00e235
singleDog <zhangjun1010031041@foxmail.com>
2017-04-20 19:54:14 +0800

    add cmn packet

--- analyzeBlueProtcol.py
+++ analyzeBlueProtcol.py
@@ -19,66 +19,6 @@ import array
 # HCI event packet
 # event_code(1 octets) + length(1 octets) + param 
 
-class HCI_Command:
-    def __init__(self, str = []):
-        self.hci_flag = []
-        self.ocf      = []
-        self.ogf      = []
-        self.length   = []
-        self.param    = []
-        self.str = str[0:]
-        self.HCICMD_init()
-        
-        
-    def HCICMD_init(self):
-        self.hci_flag = self.str[0]
-        self.ocf      = (self.str[1] & 0xc0 ) << 8 | ((self.str[1] << 2 ) | self.str[2] >> 6)
-        self.ogf      = self.str[2] & 0x3f
-        self.length   = self.str[3]
-        self.param[0:]   = self.str[4:]
-        
-        
-    def HEX_print(self):
-        for ch in self.str:
-            print hex(ch),
-        
-    def  HCI_print(self):
-        self.HEX_print()
-        print "\n|- flag # %02x"%(self.hci_flag),
-        print "|- ocf # %03x"%(self.ocf),
-        print "|- ogf # %02x"%(self.ogf),
-        print "|- len # %d"%(self.length),
-        print "|- param # ",self.param 
-        print "\n\n"
-        
-class HCI_Event:
-    def __init__(self, str = []):
-        self.str = str[0:]
-        self.EventCode = []
-        self.Length    = []
-        self.param     = []
-        
-        self.HCIEvent_Init()
-        self.HEX_Show()
-        
-        self.HCI_print()
-
-
-    def HCIEvent_Init(self):
-        self.EventCode = self.str[0]
-        self.Length = self.str[1]
-        self.param[0:] = self.str[2:]
-        
-    def HEX_Show(self):
-        for num in self.str:
-            print '0x%02x'%(num) ,
-
-    def HCI_print(self):
-        print '\n#- F -| %d'%(self.EventCode),
-        print '#- Len: %d'%(self.Length),
-        print '#- param: ' , self.param,
-        print '\n\n'
-
 def HCIEvent_print(evcode, evlen, evdata):
     print '##### Event Packet #####'
     code_num = int(evcode, 16)
@@ -125,6 +65,17 @@ def ACLPKT_print(handle, length, data):
     ACLData_Print(data)
     pass
 
+def CMD_print(opcode, cmd_len, cmd_data):
+    code_num = int(opcode[0],16) + (int(opcode[1], 16) << 8)
+    len_num  = int(cmd_len[0], 16) 
+
+    print "#### command packet ####"
+    print "code: 0x%02x len:0x%02x"%(code_num, len_num)
+    print "data: ",
+    for char in cmd_data:
+        print char,
+    print "\n\n"
+
 
 def Analyze(filename):
     fops = open(filename)
@@ -138,11 +89,21 @@ def Analyze(filename):
         split_content += line.split(" ")
     
     current_place = 0
+    
+    # print split_content
 
     while(current_place < len(split_content)):
 
         if split_content[current_place] == '01':
-            pass
+            cmd_opcode = split_content[current_place + 1: current_place + 3]
+            cmd_len = []
+            cmd_len     = split_content[current_place + 3]
+
+            len_num = int(cmd_len, 16)
+            cmd_data = split_content[current_place + 3: current_place + len_num + 3 ]
+            current_place += len_num + 4 
+
+            CMD_print(cmd_opcode, cmd_len, cmd_data)
 
         elif split_content[current_place] == '02':
             handle = split_content[current_place + 1: current_place + 3]
